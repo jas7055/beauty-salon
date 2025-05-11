afa43005db4fcdf02c8a6bce5c97ac9e83fb962f
Jasmine <jas7055@gmail.com>
2025-05-11 20:42:25 +0530

    Updates

---
+++ precache-manifest.03010d2ece9c078d8806c4793c21936e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7361bf427d86a24e9826991692509a51",
+    "url": "/beauty-salon/index.html"
+  },
+  {
+    "revision": "3b6334dbd72b9e39e171",
+    "url": "/beauty-salon/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "a07ab49e9f1bec949e97",
+    "url": "/beauty-salon/static/css/main.e6bca16f.chunk.css"
+  },
+  {
+    "revision": "3b6334dbd72b9e39e171",
+    "url": "/beauty-salon/static/js/2.429f764a.chunk.js"
+  },
+  {
+    "revision": "a07ab49e9f1bec949e97",
+    "url": "/beauty-salon/static/js/main.62dc3111.chunk.js"
+  },
+  {
+    "revision": "921eaf9cd7b1f9fe2cc1",
+    "url": "/beauty-salon/static/js/runtime-main.9dee2fe9.js"
+  }
+]);
